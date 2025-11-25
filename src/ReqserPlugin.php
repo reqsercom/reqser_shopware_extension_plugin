f9ff297fa55ff52ecddd2f32d61f2e81bc0e3c57
Joris <jorisklostermann@hotmail.com>
2025-11-25 13:45:17 +0100

    update merge main

--- src/ReqserPlugin.php
+++ src/ReqserPlugin.php
@@ -11,8 +11,6 @@ use Shopware\Core\Framework\Plugin\Context\UninstallContext;
 use Shopware\Core\Framework\Plugin\Context\UpdateContext;
 use Shopware\Core\Framework\MessageQueue\ScheduledTask\ScheduledTaskDefinition;
 use Shopware\Core\Framework\Uuid\Uuid;
-use Reqser\Plugin\Service\ScheduledTask\ReqserSnippetCrawler;
-use Reqser\Plugin\Service\ScheduledTask\ReqserSnippetCrawlerHandler;
 use Reqser\Plugin\Service\ScheduledTask\ReqserNotificiationRemoval;
 use Reqser\Plugin\Service\ScheduledTask\ReqserNotificiationRemovalHandler;
 
@@ -72,27 +70,7 @@ class ReqserPlugin extends Plugin
             '?, ?, ?, ?)
             ON DUPLICATE KEY UPDATE scheduled_task_class = VALUES(scheduled_task_class), run_interval = VALUES(run_interval), status = VALUES(status), next_execution_time = VALUES(next_execution_time), updated_at = VALUES(updated_at)';
 
-        $params = [
-            Uuid::randomBytes(),
-            ReqserSnippetCrawler::getTaskName(),
-            ReqserSnippetCrawler::class,
-            ReqserSnippetCrawler::getDefaultInterval(),
-        ];
-
-        if ($hasDefaultRunInterval) {
-            $params[] = ReqserSnippetCrawler::getDefaultInterval();
-        }
-
-        $params = array_merge($params, [
-            ScheduledTaskDefinition::STATUS_SCHEDULED,
-            $nextExecutionTime,
-            $currentTime,
-            $currentTime
-        ]);
-
-        $connection->executeStatement($sql, $params);
-
-        // Repeat for ReqserNotificationRemoval
+        // Schedule ReqserNotificationRemoval
         $params = [
             Uuid::randomBytes(),
             ReqserNotificiationRemoval::getTaskName(),
@@ -119,55 +97,21 @@ class ReqserPlugin extends Plugin
     {
         $this->container->get(Connection::class)->executeStatement(
             'DELETE FROM scheduled_task WHERE name = ?',
-            [ReqserSnippetCrawler::getTaskName()]
-        );
-        $this->container->get(Connection::class)->executeStatement(
-            'DELETE FROM scheduled_task WHERE name = ?',
             [ReqserNotificiationRemoval::getTaskName()]
         );
     }
 
     private function runTask(): void
     {
-        /** @var ReqserSnippetCrawlerHandler $handler */
-        $handler = $this->container->get(ReqserSnippetCrawlerHandler::class);
-        $handler->run();
-
         /** @var ReqserNotificiationRemovalHandler $handler */
         $handler = $this->container->get(ReqserNotificiationRemovalHandler::class);
         $handler->run();
-
     }
 
     private function shouldRunTask(string $type = 'update'): bool
     {
-        try {
-            $connection = $this->container->get(Connection::class);
-            $sql = "SELECT id, iso, custom_fields FROM snippet_set WHERE custom_fields IS NOT NULL";
-            $result = $connection->fetchAllAssociative($sql);
-
-            foreach ($result as $row) {
-                try {
-                    $customFields = json_decode($row['custom_fields'], true);
-                    if (isset($customFields['ReqserSnippetCrawl']) 
-                        && isset($customFields['ReqserSnippetCrawl']['active']) 
-                        && $customFields['ReqserSnippetCrawl']['active'] === true
-                        && isset($customFields['ReqserSnippetCrawl']['baseLanguage']) 
-                        && $customFields['ReqserSnippetCrawl']['baseLanguage'] === true
-                        && isset($customFields['ReqserSnippetCrawl']['run_on_'.$type]) 
-                        && $customFields['ReqserSnippetCrawl']['run_on_'.$type] === true
-                    ) {
-                        return true;
-                    } else {
-                    }
-                } catch (\Exception $e) {
-                    continue;
-                }
-            }
-        } catch (\Exception $e) {
-            return false;
-        }
-        return false;
+        // Always return true to run notification removal task on update/activate
+        return true;
     }
 
 }
